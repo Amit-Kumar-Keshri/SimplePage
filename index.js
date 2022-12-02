905e7dea1eecbe1245a9c5be5174e6a92c9d1834
Virus <amitkeshri003@gmail.com>
2022-12-02 20:20:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// var option = document.getElementsById("flow");
+// var arrowIcon = document.getElementById("arrowIcon");
+
+// option.onclick = function () {
+//   arrowIcon.classList.toggle("rotate");
+// };
+
+
+//  jQuery(document).ready(function () {
+//     jQuery(document).on("click", "#flow", function () {
+//       jQuery("#arrowIcon").slideToggle();
+//     });
+//  });
