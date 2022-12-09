d73d2e74d4621e2b0660c94cf5b59628db88cec3
Virus <amitkeshri003@gmail.com>
2022-12-09 20:31:57 +0530

    added scrolling effect

--- index.js
+++ index.js
@@ -6,8 +6,25 @@
 // };
 
 
-//  jQuery(document).ready(function () {
-//     jQuery(document).on("click", "#flow", function () {
-//       jQuery("#arrowIcon").slideToggle();
-//     });
-//  });
+  jQuery(document).ready(function () {
+
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+});
